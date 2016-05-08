9619575b283cf224b8d40f5863cbdce9f0ae163f
arthu <arthu@DESKTOP-MHIS9JC>
2016-05-08 16:34:02 -0300

    Refatorado remove outputs de texto da lógica do programa e adiciona regra de chave na inserção de conta corrente

--- aula-3/exercicio/main.c
+++ aula-3/exercicio/main.c
@@ -3,20 +3,29 @@
 #include "agencia.h"
 #include "contacorrente.h"
 
+ContaCorrente *buscar_conta(FILE *in,int codConta,int codAg){
+    FILE *aux = in;
+    rewind(aux);
+    ContaCorrente *cc = NULL;
+    while((cc = cc_le(aux)) != NULL && cc->cod != codConta && cc->codAgencia != codAg){
+        free(cc);
+    }
+    free(aux);
+    return cc;
+}
+
 // retorna -1 quando não é possível encontrar o código da agência referenciada
-int cadastrar_conta(FILE *in, FILE *ag){
-    int cod, codAg;
-    double saldo;
-    printf("Informe os seguintes dados da conta:\n");
-    printf("Codigo: ");
-    scanf("%d", &cod);
-    printf("Codigo da Agencia: ");
-    scanf("%d", &codAg);
-    if(buscar_agencia(ag, codAg) != 0){
+// retorna -2 quando já existe uma conta corrente com a mesma chave
+int cadastrar_conta(FILE *in, FILE *agIn,int cod,int codAg,double saldo){
+    ContaCorrente *cc;
+    Agencia ag;
+    if((cc = buscar_conta(in, cod, codAg)) != NULL){
+        free(cc);
+        return -2;
+    }
+    if(buscar_agencia(agIn, codAg) != 0){
         return -1;
     }
-    printf("Saldo em conta: ");
-    scanf("%lf", &saldo);
     ContaCorrente *new_cc = contacorrente(cod, codAg, saldo);
     cc_salva(new_cc, in);
     free(new_cc);
@@ -24,18 +33,6 @@ int cadastrar_conta(FILE *in, FILE *ag){
 }
 
 int cadastrar_agencia(FILE *in){
-    return 0;
-}
-
-int buscar_conta(FILE *in, int codConta){    
-    rewind(in);
-    ContaCorrente *cc = NULL;
-    while((cc = cc_le(in)) != NULL && cc->cod != codConta){
-        free(cc);
-    }
-    if(cc == NULL) return 1;
-    cc_imprime(cc);
-    free(cc);
     return 0;
 }
 
@@ -78,8 +75,24 @@ void main(int argc, char** argv) {
 \t2. Cadastrar uma Agencia\n\t0. Sair\n");
                             rc = scanf("%d", &resposta);           
                         }
-                        if(resposta == 1) cadastrar_conta(outConta, outAgencia);
-                        if(resposta == 2) cadastrar_agencia(outAgencia);
+                        if(resposta == 1) {
+                            int cod, codAg;
+                            double saldo;
+                            printf("Informe os seguintes dados da conta:\n");
+                            printf("Codigo: ");
+                            scanf("%d", &cod);
+                            printf("Codigo da Agencia: ");
+                            scanf("%d", &codAg);
+                            printf("Saldo em conta: ");
+                            scanf("%lf", &saldo);
+                            int r = cadastrar_conta(outConta, outAgencia, cod, codAg, saldo);
+                            if(r == -1) printf("Nao existe uma agencia com esse codigo, operacao abortada\n");
+                            else if(r == -2) printf("Ja existe uma conta com esse codigo nessa agencia\n");
+                            else printf("Conta cadastrada com sucesso!\n");
+                        }
+                        if(resposta == 2){ 
+                            cadastrar_agencia(outAgencia);
+                        }
                         break;
 
                     case 2:
@@ -90,11 +103,14 @@ void main(int argc, char** argv) {
                             rc = scanf("%d", &resposta);           
                         }
                         if(resposta == 1){
-                            int cod;
+                            int codConta, codAg;
                             printf("Digite o codigo da Conta Corrente requerida: ");
-                            scanf("%d", &cod);
-                            int r = buscar_conta(outConta, cod);
-                            if(r == 1) printf("Nao foi possivel encontrar uma conta com o codigo fornecido\n");
+                            scanf("%d", &codConta);
+                            printf("Digite o codigo da Agencia referente a Conta Corrente requerida: ");
+                            scanf("%d", &codAg);
+                            ContaCorrente *cc = buscar_conta(outConta, codConta, codAg);
+                            cc_imprime(cc);
+                            free(cc);
                         } else if(resposta == 2){
                             int cod;
                             printf("Digite o codigo da Agencia requerida: ");
