81ae8b617778f4593d0deaf9b27e49db08279e38
arthu <arthu@DESKTOP-MHIS9JC>
2016-05-13 12:03:11 -0300

    Teoricamente correto

--- aula-4/geracao_particoes.c
+++ aula-4/geracao_particoes.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
+#include <io.h>
 
 void classificacao_interna(char *nome_arquivo_entrada, Nomes *nome_arquivos_saida, int M)
 {
@@ -51,51 +52,49 @@ void selecao_com_substituicao(char *nome_arquivo_entrada, Nomes *nome_arquivos_s
 	
 	//	1. Ler M registros do arquivo para a memória
 	ler_clientes(in, &list, M, congelados);
-	
-	for(j = 0; j < conta_nomes(nome_arquivos_saida); j++){
-		out = fopen(nome_arquivos_saida->nome, "wb");
-		while(existe_nao_congelado(congelados, M)){
-			//	2. Selecionar, no array em memória, o registro r com menor chave
-			int menor_val = INT_MAX;
-			for(i = 0; i < M; i++){
-                            if(congelados[i]) continue;
-                            if(list->lista[i]->cod_cliente <= menor_val){
-                                menor_val = list->lista[i]->cod_cliente;
-                                menor = list->lista[i];
-                                m = i;
-                            }
-			}
-			//	3. Gravar o registro r na partição de saída
-                        salva_cliente(menor, out);
-                        //	4. Substituir, no array em memória, o registro r pelo próximo registro do arquivo de entrada
-                        //if(list->lista[m] != NULL){
-                        gravado = menor->cod_cliente;
-                        lido = le_cliente(in);
-                        if(lido != NULL){
-                            substituirRegistro(list->lista[m], lido);
+        int tam = conta_nomes(nome_arquivos_saida);
+	for(j = 0; j < tam; j++){
+            out = fopen(nome_arquivos_saida->nome, "wb");
+            while(existe_nao_congelado(congelados, M)){
+                    //	2. Selecionar, no array em memória, o registro r com menor chave
+                    int menor_val = INT_MAX;
+                    for(i = 0; i < M; i++){
+                        if(congelados[i]) continue;
+                        if(list->lista[i]->cod_cliente <= menor_val){
+                            menor_val = list->lista[i]->cod_cliente;
+                            menor = list->lista[i];
+                            m = i;
                         }
+                    }
+                    //	3. Gravar o registro r na partição de saída
+                    salva_cliente(menor, out);
+                    //	4. Substituir, no array em memória, o registro r pelo próximo registro do arquivo de entrada
+                    //if(list->lista[m] != NULL){
+                    gravado = menor->cod_cliente;
+                    lido = le_cliente(in);
+                    if(lido != NULL){
+                        substituirRegistro(list->lista[m], lido);
+                    }
 
-                        if(lido == NULL || lido->cod_cliente < gravado){
-                            //	5. Caso a chave deste último seja menor do que a chave recém gravada, 
-                            //	considerá-lo congelado e ignorá-lo no restante do processamento
-                            congelados[m] = 1;
-			}
-                        if(lido != NULL)
-                                free(lido);
-                        
-			//	6. Caso existam em memória registros não congelados, voltar ao passo 2
+                    if(lido == NULL || lido->cod_cliente < gravado){
+                        //	5. Caso a chave deste último seja menor do que a chave recém gravada, 
+                        //	considerá-lo congelado e ignorá-lo no restante do processamento
+                        congelados[m] = 1;
+                    }
+                    if(lido != NULL)
+                            free(lido);
+
+                    //	6. Caso existam em memória registros não congelados, voltar ao passo 2
 		}
 		/*	7. Caso contrário:
 			- fechar a partição de saída*/
-                for(i = 0; i < M; i++){
-                    if(list->lista[i] != NULL){
-                        salva_cliente(cliente(INT_MAX, ""), out);
-                        break;
-                    }
+                if(j+1<tam){
+                    salva_cliente(cliente(INT_MAX, ""), out);
                 }
 		fclose(out);
 		//	- descongelar os registros congelados
-		for(i = 0; i < M; i++) congelados[i] = 0;
+		for(i = 0; i < M; i++) 
+                    if(list->lista[i] != NULL) congelados[i] = 0;
 		//	- abrir nova partição de saída
 		nome_arquivos_saida = nome_arquivos_saida->prox;
 	}
