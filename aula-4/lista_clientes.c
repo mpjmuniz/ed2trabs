d16bdfd202865acaf64e759a2ca43082f1cb51b8
Marcelo Muniz <marcelo.pjmuniz@gmail.com>
2016-05-13 08:19:34 -0300

    Conti. Limpeza

--- aula-4/lista_clientes.c
+++ aula-4/lista_clientes.c
@@ -68,26 +68,37 @@ ListaClientes * le_clientes(char *nome_arquivo)
 }
 
 
-void ler_clientes(FILE *in, ListaClientes **list, int qtd){
-	if(qtd < 0) return;
-	if(*list == NULL){
-		int i = 0;
-		*list = (ListaClientes *) malloc(sizeof(ListaClientes));
-		if(in != NULL){
-			Cliente *cliente = NULL;
-			(*list)->qtd = qtd;
-			(*list)->lista = (Cliente **) calloc(qtd, sizeof(Cliente));
+void ler_clientes(FILE *in, ListaClientes **list, int qtd, int *congelados){
+	if(qtd < 0 || in == NULL){
+		(*list)->qtd = 0;
+		(*list)->lista = NULL;
+		return;
+	}
+	
+	int i = 0;
 
-			for(i = 0; i < qtd && (cliente = le_cliente(in)) != NULL; i++){
-				(*list)->lista[i] = cliente;
-			}
-		} else {
-			(*list)->qtd = 0;
-			(*list)->lista = NULL;
+	if(*list == NULL){
+		*list = (ListaClientes *) malloc(sizeof(ListaClientes));
+		Cliente *cliente = NULL;
+		(*list)->qtd = qtd;
+		(*list)->lista = (Cliente **) calloc(qtd, sizeof(Cliente));
+
+		for(i = 0; i < qtd && (cliente = le_cliente(in)) != NULL; i++){
+			(*list)->lista[i] = cliente;
+			congelados[i] = 0;
 		}
 	} else {
+		Cliente *cliente = NULL;
+		int qtdAntiga = (*list)->qtd;
+		(*list)->qtd += qtd;
+		(*list)->lista = (Cliente **) realloc((*list)->lista, (*list)->qtd * sizeof(Cliente));
 
+		for(i = qtdAntiga; i < (*list)->qtd && (cliente = le_cliente(in)) != NULL; i++){
+			(*list)->lista[i] = cliente;
+			congelados[i] = 0;
+		}
 	}
+	for(; i < qtd; i++) congelados[i] = 1;
 }
 
 int cmp_clientes(ListaClientes *c1, ListaClientes *c2)
