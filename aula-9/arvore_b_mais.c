5df1218e133f0252ad5661e5a3dc45a37e7dfbf3
Marcelo Muniz <marcelo.pjmuniz@gmail.com>
2016-06-22 08:16:12 -0300

    Inclusão do Exercício de Árvore B+

---
+++ aula-9/arvore_b_mais.c
@@ -0,0 +1,28 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <limits.h>
+
+#include "arvore_b_mais.h"
+
+
+int busca(int cod_cli, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados, int *pont_folha, int *encontrou)
+{
+	//TODO: Inserir aqui o codigo do algoritmo
+	*pont_folha = INT_MAX;
+	*encontrou = INT_MAX;
+    return INT_MAX;
+}
+
+int insere(int cod_cli, char *nome_cli, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados)
+{
+	//TODO: Inserir aqui o codigo do algoritmo de insercao
+    return INT_MAX;
+}
+
+int exclui(int cod_cli, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados)
+{
+	//TODO: Inserir aqui o codigo do algoritmo de remocao
+    return INT_MAX;
+}
